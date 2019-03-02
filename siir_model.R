9ffbefa2e5f9c1e857217eff4d52ec2524432d43
c0d4w4rri0r <31770513+c0d4w4rri0r@users.noreply.github.com>
2019-03-02 14:15:16 +0000

    keeping things up to date

--- siir_model.R
+++ siir_model.R
@@ -1,5 +1,6 @@
 library(deSolve)
 library(ggplot2)
+library(data.table)
 
 # system("R CMD SHLIB siir_ode.c")
 # https://doi.org/10.1038/s41564-018-0240-5
@@ -10,20 +11,21 @@ iarToR0 <- function(IAR, resistantProp = 0) {
 }
 
 plauge <- function(pop = 66000000,
-                   infA = 1000,
+                   infA = 1,
                    infB = 2.4 * 1000000, # http://dx.doi.org/10.1155/2015/504831 enough for all uni students
                    R0A = 2.011797, # spi-m
-                   R0B = 2.011797, # spi-m
-                   incA = 2,
-                   incB = 2,
-                   durA = 4,
-                   durB = 4,
+                   R0B = R0A, # spi-m
+                   incA = 0.8, # doi:10.1038/nature04017
+                   incB = incA, # doi:10.1038/nature04017
+                   durA = 1.8, # doi:10.1038/nature04017
+                   durB = durA, # doi:10.1038/nature04017
                    CRA = 0.625, # spi-m RWC
-                   CRB = 0.625, # spi-m RWC
+                   CRB = CRA, # spi-m RWC
                    CFRA = 0.025, # spi-m RWC
                    CFRB = 0.004, # spi-m 1968
                    time_to_B = 14,
-                   time_to_rep = 500 # assuming one dose
+                   deaths_till_noticed = 1,
+                   outputCurves = FALSE
 ) {
   Y <- c(S = pop - infA, EA = infA, EB = 0, IA = 0, IB = 0, RA = 0, RB = 0)
   parms <- c(
@@ -38,35 +40,84 @@ plauge <- function(pop = 66000000,
     jacfunc = "jac", dllname = "siir_ode",
     initfunc = "initmod", nout = 0
   )
-  while (times[length(times)] <= 200) {
-    times <- times[length(times)]:(times[length(times)] + time_to_rep)
-    Y <- out[[length(out)]][nrow(out[[length(out)]]), 2:8]
-    out[[length(out)]] <- out[[length(out)]][-nrow(out[[length(out)]]), ]
-    infC <- infB * Y[1] / sum(Y)
-    Y <- Y + c(-infC, 0, infC, 0, 0, 0, 0)
-    out[[length(out) + 1]] <- ode(Y, times,
-      func = "derivs", parms = parms,
-      jacfunc = "jac", dllname = "siir_ode",
-      initfunc = "initmod", nout = 0
-    )
-  }
+  times <- times[length(times)]:250
+  Y <- out[[1]][nrow(out[[1]]), 2:8]
+  out[[1]] <- out[[1]][-nrow(out[[1]]), ]
+  infC <- infB * Y[1] / sum(Y)
+  Y <- Y + c(-infC, 0, infC, 0, 0, 0, 0)
+  out[[2]] <- ode(Y, times,
+                  func = "derivs", parms = parms,
+                  jacfunc = "jac", dllname = "siir_ode",
+                  initfunc = "initmod", nout = 0
+  )
   out <- do.call(rbind, out)
   deaths <- sum(out[nrow(out), c(7, 8)] * c(CFRA, CFRB) * c(CRA, CRB))
+  endCases <- sum(out[nrow(out), c(7, 8)] * c(CRA, CRB))
   cases <- rowSums(t(t(out[, c(5, 6)]) * c(CRA, CRB)))
+  time_noticed<-which.min(abs(deaths_till_noticed-rowSums(out[, c(5, 7)]) * CFRA * CRA))
+  if (infB!=0) time_noticed<-0
   peak_time <- out[which.max(cases), 1]
-  peak_case <- cases[peak_time]
-  c(deaths = deaths, cases = sum(cases), peak_time = peak_time, peak_case = peak_case)
+  peak_case <- cases[which.max(cases)]
+  if (outputCurves) return(out)
+  else return(c(deaths = deaths, cases = endCases, peak_time = peak_time,
+                peak_case = peak_case, time_noticed = time_noticed))
 }
 
-senarios <- expand.grid(delay = seq(7, 7 * 8, 1), doses = seq(1.8 * 10^6, 2.8 * 10^6, 10^5))
+senarios <- expand.grid(
+  delay = seq(7, 7 * 8, 1),
+  doses = seq(1.8 * 10^6, 2.8 * 10^6, 10^5),
+  latentPeriod = seq(0.8, 2, 0.2),
+  infectiousPeriod = seq(1.8, 4, 0.44)
+)
 tablename <- c(colnames(senarios), names(plauge(infB = 0)))
 impact <- sapply(1:nrow(senarios), function(i) {
-  c(as.numeric(senarios[i, ]), as.numeric(plauge(infB = 0) -
-    plauge(time_to_B = senarios[i, "delay"], infB = senarios[i, "doses"])))
+  c(as.numeric(senarios[i, ]), as.numeric(plauge(
+    infB = 0, incA = senarios[i, "latentPeriod"],
+    durA = senarios[i, "infectiousPeriod"]
+  ) -
+    plauge(time_to_B = senarios[i, "delay"], infB = senarios[i, "doses"]),
+  incA = senarios[i, "latentPeriod"], durA = senarios[i, "infectiousPeriod"]
+  ))
 })
 impact <- as.data.frame(t(impact))
 colnames(impact) <- tablename
 
-impact$persentageDeathsAvoided <- 100 * impact$deaths / (plauge(infB = 0)["deaths"])
+impact$delay<-impact$delay-impact$time_noticed
+impact$latentPeriod<-as.factor(impact$latentPeriod)
+impact$infectiousPeriod<-as.factor(impact$infectiousPeriod)
 
-ggplot(data=impact)+geom_contour(aes(x=delay, y=doses, z = deaths, colour=stat(level))) + scale_colour_gradientn(colours=rainbow(5))
+refDeaths<-plauge(
+  infB = 0, incA = senarios[1, "latentPeriod"],
+  durA = senarios[1, "infectiousPeriod"]
+)["deaths"]
+
+#impact$persentageDeathsAvoided <- 100 * impact$deaths / (plauge(infB = 0)["deaths"])
+
+#contours of delay v doses for deaths
+ggplot(data = impact[impact$latentPeriod==0.8 & impact$infectiousPeriod==1.8,c("delay","doses","deaths")]) + geom_contour(aes(x = delay, y = doses, z = deaths, colour = stat(level))) + scale_colour_gradientn(colours = rainbow(5))
+#contours of delay v doses for peek cases
+ggplot(data = impact[impact$latentPeriod==0.8 & impact$infectiousPeriod==1.8,]) + geom_contour(aes(x = delay, y = doses, z = peak_case, colour = stat(level))) + scale_colour_gradientn(colours = rainbow(5))
+#contours of delay v doses for peek time
+ggplot(data = impact[impact$latentPeriod==0.8 & impact$infectiousPeriod==1.8,]) + geom_contour(aes(x = delay, y = doses, z = peak_time.time, colour = stat(level))) + scale_colour_gradientn(colours = rainbow(5))
+# deaths by delay graph for 3 difrent does levels
+ggplot(data = impact[impact$doses %in% (c(1.8, 2.4, 2.8) * 10^6) &
+                       impact$latentPeriod == 0.8 &
+                       impact$infectiousPeriod == 1.8 &
+                       impact$delay>=0, ]) + 
+  geom_path(aes(delay, deaths, colour = factor(doses / 10^6),
+                group=factor(doses / 10^6))) +
+  labs(color = "Doses") +
+  xlab("Days till vaccination") +
+  ylab("Deaths averted") +
+  scale_y_continuous(sec.axis = sec_axis(~./refDeaths, labels = scales::percent))
+# deaths by delay graph for one does but full range of latent / infectious periods
+impact2<-impact[impact$doses==2.4 * 10^6, ]
+impact2<-impact2[impact2$infectiousPeriod %in% c("1.8","4"),]
+impact2<-impact2[impact2$latentPeriod %in% c("0.8","2"),]
+ggplot(data = impact2[impact2$delay>=0,]) + 
+  geom_path(aes(delay, deaths, colour = infectiousPeriod, linetype = latentPeriod,
+                group=interaction(latentPeriod,infectiousPeriod)))
+
+impact<-as.data.table(impact)
+
+impact[,lapply(.SD, sd),by=.(delay,doses),.SDcols=names(plauge(infB = 0))]
